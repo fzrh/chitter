6f40566ef25099fe938381e2e16801cf2fa4c505
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-22 23:33:03 +0100

    Standardising string interpolation

--- spec/features/helpers/session.rb
+++ spec/features/helpers/session.rb
@@ -1,13 +1,20 @@
 module SessionHelpers
 
-  def sign_up(name = "Stephen Lloyd", username = "Stephen_lloyd", email = "stephen@makers.com", password="s3cr3t", password_confirmation = "s3cr3t")
-    visit '/makers/signup'
-    fill_in :name, with: name
+  def sign_up(name = "Stephen Lloyd", username = "Stephen_lloyd", email = "stephen@makers.com", password = "s3cr3t", password_confirmation = "s3cr3t")
+  	visit '/makers/signup'
+  	fill_in :name, with: name
+  	fill_in :username, with: username
+  	fill_in :email, with: email
+  	fill_in :password, with: password
+  	fill_in :password_confirmation, with: password_confirmation
+  	click_button 'Sign up'
+  end
+
+  def sign_in(username, password)
+    visit '/sessions/new'
     fill_in :username, with: username
-    fill_in :email, with: email
     fill_in :password, with: password
-    fill_in :password_confirmation, with: password_confirmation
-    click_button 'Sign up'
+    click_button 'Sign in'
   end
 
 end
